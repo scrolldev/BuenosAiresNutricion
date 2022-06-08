360b1e4d2e506a1427ad80733009c52710c1e19b
jonathan W. Romero <jonathanwalterromero@gmail.com>
2022-06-08 14:14:44 -0300

    Creando etiquetas de HTML en JavaScript

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,27 @@ var botonAdicionar = document.querySelector("#adicionar-paciente");
 
 botonAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log("Usted hizo clic en el titulo");
+
+    var form = document.querySelector("#form-adicionar");
+
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    console.log(nombre);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
+
+    pacienteTr = document.createElement("tr");
+    nombreTd = document.createElement("td");
+    pesoTd = document.createElement("td");
+    alturaTd = document.createElement("td");
+    gorduraTd = document.createElement("td");
+    imcTd = document.createElement("td");
+
+
 });
 
 var pacientes = document.querySelectorAll(".paciente");
