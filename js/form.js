e0f078ab218f57c144572497220f003b9879d63c
jonathan W. Romero <jonathanwalterromero@gmail.com>
2022-06-08 14:47:50 -0300

    Quebrando el código en archivos diferentes

---
+++ js/form.js
@@ -0,0 +1,32 @@
+botonAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicionar");
+
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+   var tabla = document.querySelector("#tabla-pacientes");
+
+
+    pacienteTr = document.createElement("tr");
+    nombreTd = document.createElement("td");
+    pesoTd = document.createElement("td");
+    alturaTd = document.createElement("td");
+    gorduraTd = document.createElement("td");
+    imcTd = document.createElement("td");
+
+    nombreTd.textContent = nombre;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    tabla.appendChild(pacienteTr);
+});
