c9d958d92e5f7669c79a48d42a066a8c5409d55b
jonathan W. Romero <jonathanwalterromero@gmail.com>
2022-06-10 23:22:02 -0300

    Iterando los pacientes para comparar

--- js/filtrar.js
+++ js/filtrar.js
@@ -6,5 +6,12 @@ campoFiltro.addEventListener("input",function(){
     console.log(this.value);
 
     var pacientes = document.querySelectorAll(".paciente");
-    console.log(pacientes);
+    
+
+    for(var i = 0; i < pacientes.length; i++){
+        var paciente = pacientes[i];
+        var tdNombre = paciente.querySelector(".info-nombre");
+        var nombre = tdNombre.textContent;
+        console.log(nombre);
+    }
 });
