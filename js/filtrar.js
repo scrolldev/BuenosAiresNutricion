aa27068599c2375e79ea4f209a3df4652b2e93a3
jonathan W. Romero <jonathanwalterromero@gmail.com>
2022-06-10 23:47:12 -0300

    Creando la lógica de filtros

--- js/filtrar.js
+++ js/filtrar.js
@@ -1,17 +1,27 @@
 var campoFiltro = document.querySelector("#filtrar-tabla");
-console.log(campoFiltro);
 
 campoFiltro.addEventListener("input",function(){
-    console.log("Escribieron en el input");
-    console.log(this.value);
-
     var pacientes = document.querySelectorAll(".paciente");
     
-
-    for(var i = 0; i < pacientes.length; i++){
-        var paciente = pacientes[i];
-        var tdNombre = paciente.querySelector(".info-nombre");
-        var nombre = tdNombre.textContent;
-        console.log(nombre);
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNombre = paciente.querySelector(".info-nombre");
+            var nombre = tdNombre.textContent;
+          
+    
+            if(nombre != this.value){
+                paciente.classList.add("invisible");
+            }else{
+                paciente.classList.remove("invisible");
+            }
+        }
+    }else{
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisible");
+        }
     }
+
+    
 });
